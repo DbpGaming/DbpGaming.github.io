75bb866e8c60eb374745adbeb1329849110f7670
DbpGaming <LbpDbpGaming@gmail.com>
2022-01-26 10:48:45 -0600

    redesign

--- build.sh
+++ build.sh
@@ -18,7 +18,7 @@ for FILE in $FILES0
 do
 	sed -z "s;<footer>.*</footer>;$FOOTER;g" $FILE > $TMP0
 	mv -f $TMP0 $FILE
-	sed -z "s;<nav>.*</nav>;$NAV0;g" $FILE > $TMP0
+	sed -z "s;<header>.*<img;$NAV0;g" $FILE > $TMP0
 	mv -f $TMP0 $FILE
 done
 
@@ -26,7 +26,7 @@ for FILE in $FILES1
 do
 	sed -z "s;<footer>.*</footer>;$FOOTER;g" $FILE > $TMP1
 	mv -f $TMP1 $FILE
-	sed -z "s;<nav>.*</nav>;$NAV1;g" $FILE > $TMP1
+	sed -z "s;<header>.*<img;$NAV1;g" $FILE > $TMP1
 	mv -f $TMP1 $FILE
 done
 
